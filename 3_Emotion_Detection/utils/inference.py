9af95b2f1e1c51f692c2cefae0b6c0bf97b3538e
luceious <lucysharma95@gmail.com>
2020-08-03 14:07:14 +0530

    utils  emotion detection

---
+++ 3_Emotion_Detection/utils/inference.py
@@ -0,0 +1,30 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.preprocessing import image
+
+from datetime import datetime
+
+
+def model_detection_loading(path_of_model):
+    detecn_model = cv2.CascadeClassifier(path_of_model)
+    return detecn_model
+
+def faces_detect(detecn_model, image_ary_grey):
+    return detecn_model.detectMultiScale(gray_image_ary, 1.3, 5)
+
+def bounding_box(face_coords, image_ary, clor):
+    x, y, w, h = face_coords
+    cv2.rectangle(image_ary, (x, y), (x + w, y + h), color, 2)
+
+def offsets(face_coords, offts):
+    x, y, width, height = face_coordinates
+    off_x, off_y = offts
+    return (x - off_x, x + width + off_x, y - off_y, y + height + off_y)
+
+def text_show(coords, image_ary, text, clor, ofset_x=0, ofset_y=0,
+                                                scale_font=2, thicknes=2):
+    x, y = coords[:2]
+    cv2.putText(image_ary, text, (x + ofset_x, y + ofset_y),
+                cv2.FONT_HERSHEY_SIMPLEX, 2,color, 3)
+   
---
+++ 3_Emotion_Detection/utils/preprocessor.py
@@ -0,0 +1,13 @@
+import numpy as np
+from imageio import imread
+from skimage.transform import resize
+
+def input_preproc(s, v=True):
+    s = s.astype('float32')
+    s = s / 255.0
+    if v:
+        s = s - 0.5
+        s = s * 2.0
+    return s
+
+
