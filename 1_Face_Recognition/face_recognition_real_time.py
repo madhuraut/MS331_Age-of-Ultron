1dad3c07df4cd94e5ce0a7c3bec39cf3b674db95
falguniEdge <69043105+falguniEdge@users.noreply.github.com>
2020-08-03 14:29:06 +0530

    Update face_recognition_real_time.py

--- 1_Face_Recognition/face_recognition_real_time.py
+++ 1_Face_Recognition/face_recognition_real_time.py
@@ -1,4 +1,4 @@
-#CODE FOR Face Recognition - SIH2020, Team: Age of Ultron, DIAT,Pune
+#Code For Face Recognition - SIH2020, Team: Age of Ultron, DIAT,Pune
 #importing all libraries and models
 import sys
 import time
