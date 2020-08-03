4df35bcbdee2cdce7b9136cb1a0eddf675331742
aishwarya-jhanwar-14 <69042944+aishwarya-jhanwar-14@users.noreply.github.com>
2020-08-03 14:22:27 +0530

    Update preprocessor.py

--- 3_Emotion_Detection/utils/preprocessor.py
+++ 3_Emotion_Detection/utils/preprocessor.py
@@ -1,7 +1,7 @@
 import numpy as np
 from imageio import imread
 from skimage.transform import resize
-
+##################
 def input_preproc(s, v=True):
     s = s.astype('float32')
     s = s / 255.0
